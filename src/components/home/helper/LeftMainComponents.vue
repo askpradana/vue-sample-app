1daff99583552880b445c6f2330137acb52bde07
naufal <askpradana@gmail.com>
2024-06-06 21:49:30 +0700

    feat: create watermark on client side with nodejs

--- src/components/home/helper/LeftMainComponents.vue
+++ src/components/home/helper/LeftMainComponents.vue
@@ -1,13 +1,59 @@
 <script setup>
-import Button from '../../ui/button/Button.vue'
-import { Play } from 'lucide-vue-next'
+import { ref } from 'vue'
+
+const urlUpload = import.meta.env.VITE_URL_UPLOAD
+const text = ref('')
+const image = ref('')
+const imageFile = ref(null)
+
+function handleFileChange(event) {
+  const file = event.target.files[0]
+  if (file) {
+    image.value = URL.createObjectURL(file)
+    imageFile.value = file
+  }
+}
+
+function uploadImg() {
+  if (!imageFile.value) {
+    alert('Please select an image to upload.')
+    return
+  }
+
+  const formData = new FormData()
+  formData.append('image', imageFile.value) // Append the actual file object
+  formData.append('text', text.value)
+
+  fetch(urlUpload, {
+    method: 'POST',
+    body: formData
+  })
+    .then((response) => response.blob())
+    .then((blob) => {
+      const url = window.URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.style.display = 'none'
+      a.href = url
+      a.download = text.value + '.png'
+      document.body.appendChild(a)
+      a.click()
+      window.URL.revokeObjectURL(url)
+    })
+    .catch((error) => console.error('Error:', error))
+}
 </script>
 
 <template>
   <section>
-    <p>Watermark KTP</p>
-    <Button variant="link" class="flex justify-start"
-      ><Play class="w-4 h-4 mr-2" /> Take the test</Button
-    >
+    <div v-if="!image">
+      <p>Watermark</p>
+      <input type="file" accept="image/*" @change="handleFileChange" />
+    </div>
+    <div v-if="image" class="flex flex-col gap-4">
+      <img :src="image" alt="Selected Image" />
+      <input type="text" v-model="text" placeholder="Watermark text" />
+      <button @click="image = ''">Remove image</button>
+      <button @click="uploadImg">Upload</button>
+    </div>
   </section>
 </template>

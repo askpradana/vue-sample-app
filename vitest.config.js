20a54195e7b020e0b41399531205e71e5bab7165
naufal <askpradana@gmail.com>
2024-06-13 21:18:16 +0700

    fix, base name on vitest

--- vitest.config.js
+++ vitest.config.js
@@ -10,7 +10,7 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url))
     },
-    base: '/vue-sample-app/'
+    base: '/vue-sample-app'
     // server: {
     //   cors: true,
     //   proxy: {
